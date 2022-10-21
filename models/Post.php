6d98a436b1a80fc60bbc7240bf84c4013742b0da
Ruben Lutac <ruben96_lutac@hotmail.com>
2022-10-21 19:09:14 +0200

    Delete method

--- models/Post.php
+++ models/Post.php
@@ -99,5 +99,54 @@ class Post {
         return false;
     }
 
+    public function update_post() {
+        $query = 'UPDATE '. $this-> table . 
+        ' SET 
+        title = :title,
+        body = :body,
+        author = :author,
+        category_id = :category_id
+        WHERE id = :id';
+
+        $statement = $this->conn->prepare($query);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        
+        $statement->bindParam(':id', $this->id);
+        $statement->bindParam(':title', $this->title);
+        $statement->bindParam(':body', $this->body);
+        $statement->bindParam(':author', $this->author);
+        $statement->bindParam(':category_id', $this->category_id);
+
+        if ($statement->execute()) {
+            return true;
+        }
+
+        printf('Error :%s \n', $statement->error);
+        return false;
+    }
+
+    public function delete_post() {
+        $query = 'DELETE FROM '
+        . $this->table .
+        ' WHERE id = :id';
+
+        $statement = $this->conn->prepare($query);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $statement->bindParam(':id', $this->id);
+
+        if ($statement->execute()) {
+            return true;
+        }
+
+        printf('Error :%s \n', $statement->error);
+        return false;
+    }
 }
 ?>
